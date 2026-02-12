c571a91387592f6e7c283b6adc460ae86c62eb76
dima <dimazigulin2213@gmail.com>
2026-02-12 20:27:04 +0300

    train/val/test split data

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer, BertForSequenceClassification
+from sklearn.model_selection import train_test_split
 
 
 class EmotionDataset(Dataset):
@@ -20,3 +22,30 @@ class EmotionDataset(Dataset):
         item = {k: v[idx] for k, v in self.encodings.items()}
         item["labels"] = torch.tensor(self.labels[idx])
         return item
+
+
+# dividing data
+emotion_dataset = pd.read_csv("emotion_dataset_raw.csv")
+text, emotion = emotion_dataset["Text"], emotion_dataset["Emotion"]
+
+# getting unique emotion
+unique_emotion = emotion.unique()
+label2id = {label: idx for idx, label in enumerate(unique_emotion)}
+id2label = {idx: label for label, idx in label2id.items()}
+emotion = emotion.map(label2id)
+
+# devided data into train/val/test
+train_text, temp_text, train_emotion, temp_emotion = train_test_split(
+    text,
+    emotion,
+    test_size=0.2,
+    random_state=42,
+    stratify=emotion
+)
+val_text, val_emotion, test_text, test_emotion = train_test_split(
+    temp_text,
+    temp_emotion,
+    test_size=0.5,
+    random_state=42,
+    stratify=temp_emotion
+)
